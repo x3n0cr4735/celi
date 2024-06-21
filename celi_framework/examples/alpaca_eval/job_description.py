1d14716a9cd35eb092b11ac55c2e86d88c3c0957
Abishek Chiffon <developerscut@gmail.com>
2024-06-20 20:27:29 -0400

    made example_prompt static in alpaca eval
    The first questions is not getting saved in the json after json. Task after 5 are actually not running for the first question.

--- celi_framework/examples/alpaca_eval/job_description.py
+++ celi_framework/examples/alpaca_eval/job_description.py
@@ -23,23 +23,6 @@ task_library = [
             ],
         },
     ),
-    Task(
-        task_name="Analyze and Understand the Example Prompt & Response",
-        details={
-            "description": "This task involves a critical analysis of an example response to a prompt similar to the one retrieved in the previous step. The objective is to dissect and understand the structural components, argumentation style, and evidence utilization within the example. This analysis is essential for developing a strategy that aligns with high-scoring responses.",
-            "instructions": [
-                "Retrieve an example prompt and response pair using the provided function call",
-                "Analyse the types of questions and formulate a strategy for the type of question."
-                "Examine how the response effectively addresses the instruction, noting the use of specific details and the structure of the answer."
-            ],
-            "additional_notes": [
-                "Highlight any innovative or particularly effective methods used in the example that could be adapted for your response.",
-                "You don't have to answer the question in this task, focus on understanding the example prompt and response."
-            ],
-            "tool_call": "Use a function call to retrieve_example_prompt_response and response pair for analysis.",
-            "example_call": "{{}}",
-        }
-    ),
     
     Task(
         task_name="Generate Baseline Response by answering the Current Instruction",
@@ -55,6 +38,8 @@ task_library = [
         details={
             "description": "Develop a set of verification questions that test the factual accuracy and relevance of the baseline response.",
             "instructions": [
+                "Develop a series of questions that can be used to verify the accuracy and relevance of the response.",
+                "The verification questions can also ask for sources to explain the reasoning behind the response."
                 "Review the response to ensure that it correctly interprets the instruction and provides all necessary information or answers all parts of the question."
             ],
         },
@@ -128,9 +113,19 @@ task_library = [
 
 initial_User_Message = "Welcome to your task dashboard for the Alpaca_Eval dataset. Please review the question in the dataset and start with the first uncompleted task. Focus on generating accurate and comprehensive responses. Refer to any previous tasks if needed to maintain continuity and accuracy."
 
-pre_algo_instruct = "Before we start answering questions from the Alpaca_Eval dataset, ensure you understand the context and requirements of each question. You will be provided with the question and expected to research and draft a response based on reliable sources. Pay close attention to the specifics of each question to tailor your responses appropriately."
+pre_algo_instruct = """Before we start answering questions from the Alpaca_Eval dataset, ensure you understand the context and requirements of each question. You will be provided with the question and expected to research and draft a response based on reliable sources. Pay close attention to the specifics of each question to tailor your responses appropriately.
 
-post_algo_instruct = "After drafting your response, review it against the example outputs provided for quality and comprehensiveness. Ensure your final response aligns with the expected format and detail as illustrated by successful examples. Reflect on any feedback or revisions suggested in the dataset guidelines to optimize your response before submission. Then go to the next question and repeat the process if there are question left"
+    Analyze the Example Instruction and Output:
+    Analyse the types of questions and formulate a strategy for the type of question.
+    Examine how the response effectively addresses the instruction, noting the use of specific details and the structure of the answer.
+    
+    Example Instruction & Output:
+      "1":{
+    "instruction": "Marie is at the music store for a store day. She bought two $50 combos. Then, for $25 she bought two t-shirts for $5 each and five hoodies for $2 each. Then, she bought sneakers for $8 each. How much did she spend total?",
+    "output": "Marie spent a total of $147."}
+     """
+
+post_algo_instruct = "After drafting your response, review it against the example outputs provided for quality and comprehensiveness. Ensure your final response aligns with the expected format and detail as illustrated by successful examples. Reflect on any feedback or revisions suggested in the dataset guidelines to optimize your response before submission."
     
 system_message = """
     As an AI trained to assist with the answering questions, your goal is to generate concise and precise answers to the provided instructions.
